8ce44a785f43e19a5e33515decd81bb6cbf2b6c8
Elisa Rubeo <rubeo.elisa4@gmail.com>
2025-09-01 16:04:54 +0200

    no message

---
+++ siwproject/src/main/java/it/uniroma3/siw/SiwprojectApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwprojectApplication.class, args);
+	}
+
+}
